97784f0da735b81260aa5bdfbc354ea89c06bd3e
bugger <usernamesarenoteasy@proton.me>
2024-05-05 10:24:10 -0600

    added testing framework

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,29 +4,21 @@
 #![test_runner(rustos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-
 use core::panic::PanicInfo;
-use rustos::println;
-use rustos::print;
+use rustos::{println, print};
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello, World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rustos::test_panic_handler(info)
